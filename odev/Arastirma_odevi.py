35cece5dcc95ad794ae8ccb09d248bd9fde434ac
kodlabjuniorofficial <kodlabjuniorofficial@gmail.com>
2026-02-01 14:08:35 +0300

    Add research assignment template in Arastirma_odevi.py

---
+++ odev/Arastirma_odevi.py
@@ -0,0 +1,22 @@
+"""
+! Yapman Gereken Araştırma ödevi
+
+Telegram botuna derste kullanmadığın bir konu sor.
+
+   ? /nedir konu komutunu kullan.
+
+Gelen bilgiyi kendi cümlelerinle kısaca özetle.
+
+Örnek Komut
+   ? /nedir Mars
+
+! Yazım Formatı
+
+KONU:
+Mars
+
+! ÖZET:
+Buraya botun verdiği bilgiden anladıklarını
+kendi cümlelerinle 3–5 satır olacak şekilde yaz.
+
+"""
